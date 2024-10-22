eb7467533bccdb024cacef7e1e99ba7cf809e16e
bolaji2274 <hammedbolajihammed@gmail.com>
2024-10-22 19:30:01 +0100

    file addee

--- backend/jwt_auth/build_files.sh
+++ backend/jwt_auth/build_files.sh
@@ -1,15 +1,10 @@
-python3 -m venv env
+ python3 -m venv env
 source env/bin/activate
 
 pip install -r requirement.txt 
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic
 # Run collectstatic to gather static files
 # python manage.py collectstatic --noinput
 
 # Create the build folder if it doesn't exist
-mkdir -p staticfiles_build
-
-# Move static files into the expected folder
-cp -r staticfiles/* staticfiles_build/
-
 # export DATABASE_URL='postgresql://postgres:rsryyszTqRCPNagcSDTqOhzmuwXLZZUf@junction.proxy.rlwy.net:31419/railway'
