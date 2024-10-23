564e0276bb26ece66a667f4d366f2c2ac8bfdc17
bolaji2274 <hammedbolajihammed@gmail.com>
2024-10-23 18:54:27 +0100

    file added

--- frontend/src/components/users/UsersTable.jsx
+++ frontend/src/components/users/UsersTable.jsx
@@ -3,11 +3,11 @@ import { motion } from "framer-motion";
 import { Search } from "lucide-react";
 
 const userData = [
-	{ id: 1, name: "John Doe", email: "john@example.com", role: "Customer", status: "Active" },
-	{ id: 2, name: "Jane Smith", email: "jane@example.com", role: "Admin", status: "Active" },
-	{ id: 3, name: "Bob Johnson", email: "bob@example.com", role: "Customer", status: "Inactive" },
-	{ id: 4, name: "Alice Brown", email: "alice@example.com", role: "Customer", status: "Active" },
-	{ id: 5, name: "Charlie Wilson", email: "charlie@example.com", role: "Moderator", status: "Active" },
+	{ id: 1, name: "Adekunle Tijani", email: "tijani@example.com", role: "Customer", status: "Active" },
+	{ id: 2, name: "Bolaji Hammed", email: "hammedbolajihammed@gmail.com", role: "Admin", status: "Active" },
+	{ id: 3, name: "Zakarriya Alliyy", email: "bob@example.com", role: "Customer", status: "Inactive" },
+	// { id: 4, name: "Alice Brown", email: "alice@example.com", role: "Customer", status: "Active" },
+	// { id: 5, name: "Charlie Wilson", email: "charlie@example.com", role: "Moderator", status: "Active" },
 ];
 
 const UsersTable = () => {
